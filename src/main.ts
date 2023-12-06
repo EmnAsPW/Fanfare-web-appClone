3a764b0e38128706b5dfab25bb4197e19d26459c
EmnAsPW <emonsourov@gmail.com>
2023-12-06 09:26:21 +0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://127.0.0.1:5174',
+    credentials: true,
+    // all headers that client are allowed to use
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  });
+  app.use(cookie.middleware());
+  await app.listen(3000);
+}
+bootstrap();
+
+//emonsourov  sHkK78OYYepWEqZq
